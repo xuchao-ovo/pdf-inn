e264888c7c4145f1fba8d0f1f7f166cdfeef30e9
xuchao <879642177@qq.com>
2023-04-07 10:58:53 +0800

    fix: 优化信息对比代码

--- verify-data-unique.ts
+++ verify-data-unique.ts
@@ -1,24 +1,41 @@
-import XLSX from 'xlsx';
-const workbook = XLSX.readFile('INN-tib-Lists.xlsx');
+import XLSX from "xlsx";
+const workbook = XLSX.readFile("INN-tib-Lists.xlsx");
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
 // 获取所有单元格的值
 // rome-ignore lint/suspicious/noExplicitAny: <explanation>
-const  data:any = XLSX.utils.sheet_to_json(worksheet, { header: 1, blankrows: false });
+const data: any = XLSX.utils.sheet_to_json(worksheet, {
+  header: 1,
+  blankrows: false,
+});
 
 // 单词
-const word = 'ldhwdt';
-
+//const word = "ldhwdt";
+const words = [
+  "rasem",
+  "racon",
+  "ire",
+  "conpel",
+  "susalt",
+  "sutal",
+  "licon",
+  "lipol",
+  "selco",
+];
 // 比较单词和 Excel 表格中的数据
-for (let i = 1; i < data.length; i++) {
-  const value = data[i][0].toString();
+words.forEach((word, index) => {
+  const log = `正在匹配第${index}个名称{ ${word}tinib }，重复数据如下：`;
+  console.log(log);
+  for (let i = 1; i < data.length; i++) {
+    const value = data[i][0].toString();
 
-  for (let j = 0; j < word.length - 1; j++) {
-    const pair = word.substring(j, j + 2);
+    for (let j = 0; j < word.length - 1; j++) {
+      const pair = word.substring(j, j + 2);
 
-    if (value.includes(pair)) {
-      console.log(value);
-      break;
+      if (value.includes(pair)) {
+        console.log(value);
+        break;
+      }
     }
   }
-}
+});
