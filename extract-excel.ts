ccf37eac00d54f765ce36d97c61979cb044c59eb
xuchao <879642177@qq.com>
2023-04-07 10:35:07 +0800

    doc: 新增提取pdf源信息文档

--- extract-excel.ts
+++ extract-excel.ts
@@ -6,11 +6,11 @@ const workbook = new ExcelJS.Workbook();
 // 创建一个工作表
 const worksheet = workbook.addWorksheet("INN-tib");
 
-const fileContent = fs.readFileSync("pls.txt", "utf8");
+const fileContent = fs.readFileSync("/docs/pls.txt", "utf8");
 
 //const words = fileContent.match(/[a-zA-Z]+/g);
 //const words = fileContent.match(/\b[a-zA-Z]*tinib\b/g);
-const words =[...new Set(fileContent.match(/\b\S*tinib\b/g))];
+const words =[...new Set(fileContent.match(/\b\S*sib\b/g))];
 
 
 console.log(words);
@@ -23,7 +23,7 @@ if (words) {
 
 // 保存工作簿为Excel文件
 workbook.xlsx
-  .writeFile("INN-tib-Lists.xlsx")
+  .writeFile("INN-sib-Lists.xlsx")
   .then(function () {
     console.log("Excel文件已成功保存！");
   })
