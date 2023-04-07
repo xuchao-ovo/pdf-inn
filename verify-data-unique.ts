e40b287354b62c9cf760f1852b9bbd12546639cb
xuchao <xuchao@illusiontech.cn>
2023-04-07 17:07:53 +0800

    fix: 修改格式

--- verify-data-unique.ts
+++ verify-data-unique.ts
@@ -1,5 +1,5 @@
 import XLSX from "xlsx";
-const workbook = XLSX.readFile("INN-tib-Lists.xlsx");
+const workbook = XLSX.readFile("INN-tinib-Lists.xlsx");
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
 // 获取所有单元格的值
@@ -12,19 +12,20 @@ const data: any = XLSX.utils.sheet_to_json(worksheet, {
 // 单词
 //const word = "ldhwdt";
 const words = [
-  "rasem",
-  "racon",
-  "ire",
-  "conpel",
-  "susalt",
-  "sutal",
-  "licon",
-  "lipol",
-  "selco",
+  // "rasem",
+  // "racon",
+  // "ire",
+  // "conpel",
+  // "susalt",
+  // "sutal",
+  // "licon",
+  // "lipol",
+  // "selco",
+  "lifcon"
 ];
 // 比较单词和 Excel 表格中的数据
 words.forEach((word, index) => {
-  const log = `正在匹配第${index}个名称{ ${word}tinib }，重复数据如下：`;
+  const log = `正在匹配第${index + 1}个名称{ ${word}tinib }，重复数据如下：`;
   console.log(log);
   for (let i = 1; i < data.length; i++) {
     const value = data[i][0].toString();
